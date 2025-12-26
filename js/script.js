e26f4f080ef8a692e758d998c5006345ac15b9d0
OvMikaelson <abdelkoddous.laarif@gmail.com>
2025-12-27 00:22:38 +0100

    added gallery

--- js/script.js
+++ js/script.js
@@ -21,6 +21,8 @@
 			copyrightYear: $(".copyright-year"),
 			materialParallax: $(".parallax-container"),
 			preloader: $(".preloader"),
+			wow: $( ".wow" ),
+			isotope: $( ".isotope" ),
 			rdNavbar: $(".rd-navbar"),
 			rdMailForm: $(".rd-mailform"),
 			rdInputLabel: $(".form-label"),
@@ -300,6 +302,71 @@
 
 		// WOW
 		if ($html.hasClass("wow-animation") && plugins.wow.length && !isNoviBuilder && isDesktop) {
+			new WOW().init();
+		}
+		
+		// Isotope
+		if ( plugins.isotope.length ) {
+			var isogroup = [];
+			for ( var i = 0; i < plugins.isotope.length; i++ ) {
+				var isotopeItem = plugins.isotope[ i ],
+					isotopeInitAttrs = {
+						itemSelector: '.isotope-item',
+						layoutMode:   isotopeItem.getAttribute( 'data-isotope-layout' ) ? isotopeItem.getAttribute( 'data-isotope-layout' ) : 'masonry',
+						filter:       '*'
+					};
+
+				if ( isotopeItem.getAttribute( 'data-column-width' ) ) {
+					isotopeInitAttrs.masonry = {
+						columnWidth: parseFloat( isotopeItem.getAttribute( 'data-column-width' ) )
+					};
+				} else if ( isotopeItem.getAttribute( 'data-column-class' ) ) {
+					isotopeInitAttrs.masonry = {
+						columnWidth: isotopeItem.getAttribute( 'data-column-class' )
+					};
+				}
+
+				var iso = new Isotope( isotopeItem, isotopeInitAttrs );
+				isogroup.push( iso );
+			}
+
+
+			setTimeout( function () {
+				for ( var i = 0; i < isogroup.length; i++ ) {
+					isogroup[ i ].element.className += " isotope--loaded";
+					isogroup[ i ].layout();
+				}
+			}, 200 );
+
+			var resizeTimout;
+
+			$( "[data-isotope-filter]" ).on( "click", function ( e ) {
+				e.preventDefault();
+				var filter = $( this );
+				clearTimeout( resizeTimout );
+				filter.parents( ".isotope-filters" ).find( '.active' ).removeClass( "active" );
+				filter.addClass( "active" );
+				var iso = $( '.isotope[data-isotope-group="' + this.getAttribute( "data-isotope-group" ) + '"]' ),
+					isotopeAttrs = {
+						itemSelector: '.isotope-item',
+						layoutMode:   iso.attr( 'data-isotope-layout' ) ? iso.attr( 'data-isotope-layout' ) : 'masonry',
+						filter:       this.getAttribute( "data-isotope-filter" ) === '*' ? '*' : '[data-filter*="' + this.getAttribute( "data-isotope-filter" ) + '"]'
+					};
+				if ( iso.attr( 'data-column-width' ) ) {
+					isotopeAttrs.masonry = {
+						columnWidth: parseFloat( iso.attr( 'data-column-width' ) )
+					};
+				} else if ( iso.attr( 'data-column-class' ) ) {
+					isotopeAttrs.masonry = {
+						columnWidth: iso.attr( 'data-column-class' )
+					};
+				}
+				iso.isotope( isotopeAttrs );
+			} ).eq( 0 ).trigger( "click" )
+		}
+
+		// WOW
+		if ( $html.hasClass( "wow-animation" ) && plugins.wow.length && !isNoviBuilder && isDesktop ) {
 			new WOW().init();
 		}
 
